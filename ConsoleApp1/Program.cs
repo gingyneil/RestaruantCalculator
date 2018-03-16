e4c3468109eef6f6b7b066756b8211d3e80e58a4
gingyneil <neil.reading.nr@gmail.com>
2018-03-16 11:28:00 -0400

    Complete. Must take 3 inputs from user. 1-mealcost 2-tax amount 3-tip amount

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RestaurantCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            //my code
+            double totalTip = CalculateTip(meal_cost, tip_percent);
+            double totalTax = CalculateTax(meal_cost, tax_percent);
+
+            double total = meal_cost + totalTax + totalTip;
+
+           total = Math.Round(total);
+
+            System.Console.Write($"the total cost for the meal is {total}");
+
+            System.Console.ReadLine();
+        }
+
+        public static double CalculateTip(double meal_cost, int tip_percent)
+        {
+            double costOfTip = meal_cost * ((double)tip_percent / 100);
+
+            return costOfTip;
+        }
+
+        public static double CalculateTax(double meal_cost, int tax_percent)
+        {
+            double costOfTax = meal_cost * ((double)tax_percent / 100);
+
+            return costOfTax;
         }
     }
 }
